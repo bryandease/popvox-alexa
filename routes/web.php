97ff8abf3c28b5d0eebe14a8652a4f4073ecf994
bryandease <bryan@bryandease.com>
2016-12-20 15:39:28 -0600

    Testing this out

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ Route::get('/', function () {
 
 AlexaRoute::intent('/request', 'GetLatestBills', function(){
     \Log::info('I got a request');
-    return Alexa::say('Proposing an amendment to the Constitution of the United States to treat Puerto Rico as if it were a State for purposes of the election of the President and Vice President.');
+    return Alexa::say('The latest bill at POPVOX is house joint resolution 104 : Proposing an amendment to the Constitution of the United States to treat Puerto Rico as if it were a State for purposes of the election of the President and Vice President.  This bill is sponsored by Representative Steve Cohen of Tennessee\'s 9th district.');
 });
