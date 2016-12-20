28d1270adf27c5285b5c7ffc4ff1ef231c79044c
bryandease <bryan@bryandease.com>
2016-12-20 17:18:47 -0600

    Testing bill sentiment

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-AlexaRoute::intent('/request', 'GetLatestBills', function(){
-    \Log::info('I got a request');
+AlexaRoute::intent('/request', 'GetLatestBill', function(){
     return Alexa::say('The latest bill at POPVOX is house joint resolution 104 : Proposing an amendment to the Constitution of the United States to treat Puerto Rico as if it were a State for purposes of the election of the President and Vice President.  This bill is sponsored by Representative Steve Cohen of Tennessee\'s 9th district.');
+});
+
+AlexaRoute::intent('/request', 'GetBillSentiment', function(){
+    return Alexa::say('38 percent of people oppose this bill and 62 percent of people support this bill');
 });
