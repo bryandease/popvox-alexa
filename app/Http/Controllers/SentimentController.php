d20b31c4862b095c38cd8bdb12098d40fad2aee8
bryandease <bryan@bryandease.com>
2016-12-20 20:29:35 -0600

    Creating sentiment controller

--- app/Http/Controllers/SentimentController.php
+++ app/Http/Controllers/SentimentController.php
@@ -1,14 +1,35 @@
 <?php namespace App\Http\Controllers;
 
 use Alexa;
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
 class SentimentController extends Controller
 {
     public function getBillSentiment()
     {
-        $bill_type = Alexa::slot('BillType');
-        $bill_number = Alexa::slot('BillType');
-        return Alexa::say('38 percent of people oppose this bill and 62 percent of people support this bill');
+//        $bill_type = Alexa::slot('BillType');
+//        $bill_number = Alexa::slot('BillNumber');
+
+        $bill_type = request('BillType');
+        $bill_number = request('BillNumber');
+
+        $client = new Client();
+        $url = 'https://www.popvox.com/api/bill-sentiment/114/' . $bill_type . $bill_number . '?pvoxKey=4a849e323b72398c498f546a047a4451b788a446';
+        $res = $client->request('GET', $url);
+
+        $body = json_decode($res->getBody());
+
+        $bill_info = $body->data;
+
+        $total_users = $bill_info->POPVOX_users_supporting + $bill_info->POPVOX_users_opposing;
+        $support_percentage =  $total_users > 0 ? round((!empty($bill_info->POPVOX_users_supporting) ? count($bill_info->POPVOX_users_supporting) : 0) / $total_users * 100, 0) : 0;
+        $oppose_percentage =  $total_users > 0 ? round((!empty($bill_info->POPVOX_users_opposing) ? count($bill_info->POPVOX_users_opposing) : 0) / $total_users * 100, 0) : 0;
+
+        $alexa_speech = $bill_info->bill_number . ' ' . $bill_info->bill_name;
+        $alexa_speech .= ' <break time="2s"/> ';
+        $alexa_speech .= $support_percentage . ' percent of people support this bill and ' . $oppose_percentage . ' others oppose it.';
+
+        return Alexa::say($alexa_speech);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/test', 'SentimentController@getBillSentiment');
+
 AlexaRoute::intent('/request', 'GetLatestBill', function() {
     return Alexa::say('The latest bill at POPVOX is house joint resolution 104 : Proposing an amendment to the Constitution of the United States to treat Puerto Rico as if it were a State for purposes of the election of the President and Vice President.  This bill is sponsored by Representative Steve Cohen of Tennessee\'s 9th district.');
 });
